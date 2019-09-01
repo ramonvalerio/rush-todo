4a5f8c6ced0498faf1d587decf1f3faad9747818
Ramon Valerio <ramonvalerios@gmail.com>
2019-09-02 11:14:49 +1200

    Saga configuration

--- src/mofo.todo.core/Application/Todos/TodoAppService.cs
+++ src/mofo.todo.core/Application/Todos/TodoAppService.cs
@@ -1,4 +1,6 @@
-﻿using mofo.todo.Core.Domain.Todos;
+﻿using mofo.todo.Core.Commands;
+using mofo.todo.Core.Domain.Todos;
+using NServiceBus;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,10 +9,20 @@ namespace mofo.todo.core.Application
     public class TodoAppService : ITodoAppService
     {
         private readonly ITodoService _todoService;
+        private readonly EndpointConfiguration _endPointConfiguration;
 
         public TodoAppService(ITodoService todoService)
         {
             _todoService = todoService;
+
+            _endPointConfiguration = new EndpointConfiguration("Mofo.Todo");
+            _endPointConfiguration.UsePersistence<NHibernatePersistence>();
+            _endPointConfiguration.EnableInstallers();
+            _endPointConfiguration.SagaPlugin("OpenPlatform.ServiceControl");
+            _endPointConfiguration.SendFailedMessagesTo("ExemploSaga.Pedidos.Errors");
+
+            var transport = _endPointConfiguration.UseTransport<RabbitMQTransport>();
+            transport.ConnectionString("host=localhost");
         }
 
         public async Task<IEnumerable<TodoItem>> GetAllAsync()
@@ -25,11 +37,10 @@ namespace mofo.todo.core.Application
 
         public async Task<TodoItem> AddAsync(string title, string status)
         {
-            //var command = new CreateTodoItem(title, status);
-            //await _busClient.PublishAsync(command);
+            var command = new CreateTodoItem(title, status);
+            //await _endPointConfiguration.SendLocal(command);
 
-            var todoItem = new TodoItem(title, status);
-            return await _todoService.AddAsync(todoItem);
+            throw new System.Exception();
         }
 
         public async Task<TodoItem> UpdateAsync(int todoId, string title, string status)
--- src/mofo.todo.core/Domain/Todos/TodoSaga.cs
+++ src/mofo.todo.core/Domain/Todos/TodoSaga.cs
@@ -1,14 +1,28 @@
-﻿using mofo.todo.Core.Commands;
+﻿using System.Threading.Tasks;
+using mofo.todo.Core.Commands;
+using mofo.todo.Core.Events;
 using NServiceBus;
 
 namespace mofo.todo.core.Domain.Todos
 {
-    public class TodoSaga : Saga<TodoSagaData>
+    public class TodoSaga : Saga<TodoSagaData>,
+        IAmStartedByMessages<CreateTodoItem>,
+        IHandleMessages<TodoItemCreated>
     {
         protected override void ConfigureHowToFindSaga(SagaPropertyMapper<TodoSagaData> mapper)
         {
             mapper.ConfigureMapping<CreateTodoItem>(message => message.Id).ToSaga(saga => saga.TodoId);
             mapper.ConfigureMapping<CreateTodoItem>(message => message.Title).ToSaga(saga => saga.TodoTitle);
+        }
+
+        public Task Handle(TodoItemCreated message, IMessageHandlerContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Handle(CreateTodoItem message, IMessageHandlerContext context)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
